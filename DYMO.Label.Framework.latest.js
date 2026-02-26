d6b1925ef3debe3a5bc601acb02a5195dd0f332e
jonnyrallus <jonnyrallus@gmail.com>
2026-02-26 11:43:25 +0200

    Add die-cut label XML configuration
    Added XML structure for a die-cut label configuration.

---
+++ DYMO.Label.Framework.latest.js
@@ -0,0 +1,43 @@
+// Source - https://stackoverflow.com/q/31697388
+// Posted by Dean.DePue
+// Retrieved 2026-02-26, License - CC BY-SA 3.0
+
+ '<?xml version="1.0" encoding="utf-8"?>\
+                    <DieCutLabel Version="8.0" Units="twips">\
+                        <PaperOrientation>Landscape</PaperOrientation>\
+                        <Id>Address</Id>\
+                        <PaperName>30252 Address</PaperName>\
+                        <DrawCommands>\
+                            <RoundRectangle X="0" Y="0" Width="1581" Height="5040" Rx="270" Ry="270" />\
+                        </DrawCommands>\
+                        <ObjectInfo>\
+                            <AddressObject>\
+                                <Name>Address</Name>\
+                                <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />\
+                                <BackColor Alpha="0" Red="255" Green="255" Blue="255" />\
+                                <LinkedObjectName></LinkedObjectName>\
+                                <Rotation>Rotation0</Rotation>\
+                                <IsMirrored>False</IsMirrored>\
+                                <IsVariable>True</IsVariable>\
+                                <HorizontalAlignment>Left</HorizontalAlignment>\
+                                <VerticalAlignment>Middle</VerticalAlignment>\
+                                <TextFitMode>ShrinkToFit</TextFitMode>\
+                                <UseFullFontHeight>True</UseFullFontHeight>\
+                                <Verticalized>False</Verticalized>\
+                                <StyledText>\
+                                    <Element>\
+                                        <Attributes>\
+                                            <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />\
+                                            <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />\
+                                        </Attributes>\
+                                    </Element>\
+                                </StyledText>\
+                                <LineFonts>\
+                                    <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />\
+                                    <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />\
+                                    <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />\
+                                </LineFonts>\
+                            </AddressObject>\
+                            <Bounds X="332" Y="150" Width="4455" Height="1260" />\
+                        </ObjectInfo>\
+                    </DieCutLabel>'
